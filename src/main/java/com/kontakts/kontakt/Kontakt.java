a99dda4ac12a9bdd7d0a3dacf0a4aa040796dbea
parkerlewis9 <parker.lewis14@att.net>
2018-05-05 10:44:41 -0500

    implemented Kontact model

--- src/main/java/com/kontakts/kontakt/Kontakt.java
+++ src/main/java/com/kontakts/kontakt/Kontakt.java
@@ -1,5 +1,94 @@
 package com.kontakts.kontakt;
 
 public class Kontakt {
-    
+
+    private String name;
+
+    private String phoneNumber;
+
+    private String emailAddress;
+
+    private String addressLineOne;
+    private String addressLineTwo;
+    private String state;
+    private String country;
+    private String zipcode;
+
+    public Kontakt(String name, String phoneNumber, String emailAddress, String addressLineOne,
+                   String addressLineTwo, String state, String country, String zipcode) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+        this.addressLineOne = addressLineOne;
+        this.addressLineTwo = addressLineTwo;
+        this.state = state;
+        this.country = country;
+        this.zipcode = zipcode;
+    }
+
+    public Kontakt() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getAddressLineOne() {
+        return addressLineOne;
+    }
+
+    public void setAddressLineOne(String addressLineOne) {
+        this.addressLineOne = addressLineOne;
+    }
+
+    public String getAddressLineTwo() {
+        return addressLineTwo;
+    }
+
+    public void setAddressLineTwo(String addressLineTwo) {
+        this.addressLineTwo = addressLineTwo;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
 }
