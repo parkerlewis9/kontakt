113034a175146b8141dd0f8313ea628ac6c22f71
parkerlewis9 <parker.lewis14@att.net>
2018-05-05 15:57:26 -0500

    added show route

--- src/main/java/com/kontakt/controller/KontaktController.java
+++ src/main/java/com/kontakt/controller/KontaktController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import java.lang.reflect.Array;
 import java.util.List;
 
 @RestController
@@ -29,12 +30,14 @@ public class KontaktController {
 
         return new ResponseEntity<>(kontakts, HttpStatus.OK);
     }
-//
-//    @RequestMapping(value = "/api/kontakt/{id}", method = RequestMethod.GET)
-//    public ResponseEntity<Kontakt> showKontakt(@PathVariable("id") String id) {
-//        // look up by id and return
-//    }
-//
+
+    @RequestMapping(value = "/api/kontakts/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Kontakt> showKontakt(@PathVariable("id") String id) {
+
+        Kontakt kontakt = repository.findById(Long.parseLong(id)).get();
+        return new ResponseEntity<>(kontakt, HttpStatus.OK);
+    }
+
     @RequestMapping(value = "/api/kontakts", method = RequestMethod.POST)
     public ResponseEntity createKontakt(@RequestBody Kontakt kontakt) {
         try {
@@ -46,7 +49,7 @@ public class KontaktController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 //
-//    @RequestMapping(value = "/api/kontakt/{id}", method = RequestMethod.PUT)
+//    @RequestMapping(value = "/api/kontakts/{id}", method = RequestMethod.PUT)
 //    public ResponseEntity<Kontakt> updateKontakt(@PathVariable("id") String id, @RequestBody Kontakt kontakt) {
 //        // update and return
 //    }
--- src/main/java/com/kontakt/repository/KontaktRepository.java
+++ src/main/java/com/kontakt/repository/KontaktRepository.java
@@ -2,8 +2,9 @@ package com.kontakt.repository;
 
 import com.kontakt.bean.Kontakt;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface KontaktRepository extends CrudRepository<Kontakt, Long> {
+public interface KontaktRepository extends PagingAndSortingRepository<Kontakt, Long> {
 }
